6711cbeb4db7fa873f5c2cac754654870b08da81
Fausto Llapa <faustollapa850@gmail.com>
2023-07-11 14:12:10 -0400

    Latest commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
         setLoading(true);
 
         try {
-            const response = await fetch('https://ai-image-generator-z3kn.onrender.com', {
+            const response = await fetch("https://ai-image-generator-z3kn.onrender.com", {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
